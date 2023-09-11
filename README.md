cc32584f6022a6236f5bcac1147429c335179031
mphoshabalala3401@gmail.com <mphoshabalala3401@gmail.com>
2023-09-11 06:42:31 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,110 @@
+Sure, here's a README documentation based on your provided code:
+
+---
+
+# Shopping List App
+
+## Overview
+
+This is a simple shopping list application built using React. It allows users to add items to their shopping list, set a budget, and track the total cost of items in their list. Additionally, users can mark items as purchased or unpurchased, and the application will calculate the current cost based on their selections.
+
+## Features
+
+- Add items to the shopping list with a name and price.
+- Set a budget to track your spending.
+- Mark items as purchased or unpurchased with a click.
+- Automatically calculate the total price of all items in the list.
+- Display the current cost based on purchased items.
+- Display the budget and remaining budget after making purchases.
+
+## Getting Started
+
+To get started with the Shopping List App, follow these steps:
+
+1. Clone this repository to your local machine:
+
+   ```shell
+   git clone https://github.com/your-username/shopping-list-app.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```shell
+   cd shopping-list-app
+   ```
+
+3. Install the required dependencies:
+
+   ```shell
+   npm install
+   ```
+
+4. Start the development server:
+
+   ```shell
+   npm start
+   ```
+
+5. Open your web browser and access the app at [http://localhost:3000](http://localhost:3000).
+
+## Usage
+
+### Adding Items
+
+- To add an item to your shopping list, use the "Add to list🛒" form.
+- Enter the item name and its price, then click the "Add to list🛒" button.
+- The item will be added to your shopping list with a default "unpurchased" status.
+
+### Setting a Budget
+
+- You can set a budget by entering a numeric value in the "Budget" input field and clicking the "Set budget💵" button.
+- The budget will be displayed in the "My Budget" field, prefixed with "R" (for example, "R100.00").
+
+### Managing Items
+
+- In the shopping list table, you'll see the names, prices, and "Purchased" status of each item.
+- To mark an item as purchased or unpurchased, click the "✅" or "❌" icon in the "Purchased" column.
+- The "Current Cost" field will update to reflect the cost of purchased items.
+
+### Viewing Total Price
+
+- The "Total Price" field displays the total price of all items in your shopping list, regardless of their purchased status.
+- The total price is displayed with "R" as a prefix (for example, "R250.00").
+
+## Contributing
+
+If you'd like to contribute to this project, please follow these guidelines:
+
+1. Fork the repository on GitHub.
+
+2. Clone your forked repository to your local machine.
+
+3. Create a new branch for your changes:
+
+   ```shell
+   git checkout -b feature/my-feature
+   ```
+
+4. Make your changes and commit them with descriptive commit messages:
+
+   ```shell
+   git commit -m "Add new feature: my feature"
+   ```
+
+5. Push your changes to your GitHub repository:
+
+   ```shell
+   git push origin feature/my-feature
+   ```
+
+6. Create a pull request from your branch to the original repository's `main` branch.
+
+7. Describe your changes and submit the pull request.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+---
+
+Feel free to customize this README to include any additional information or specific instructions for your project.
---
+++ src/App.js
@@ -0,0 +1,181 @@
+import { useState } from "react";
+
+const items = [
+  { id: 1, itemName: "Apples", itemPrice: 56.81, purchased: false },
+  { id: 2, itemName: "Bananas", itemPrice: 28.31, purchased: false },
+  { id: 3, itemName: "Milk", itemPrice: 66.31, purchased: false },
+  { id: 4, itemName: "Eggs", itemPrice: 43.51, purchased: false },
+  { id: 5, itemName: "Bread", itemPrice: 56.81, purchased: false },
+  { id: 6, itemName: "Chicken Breast", itemPrice: 113.81, purchased: false },
+  { id: 7, itemName: "Pasta", itemPrice: 34.01, purchased: false },
+  { id: 8, itemName: "Rice", itemPrice: 47.31, purchased: false },
+  { id: 9, itemName: "Tomatoes", itemPrice: 37.81, purchased: false },
+  { id: 10, itemName: "Spinach", itemPrice: 43.51, purchased: false },
+];
+
+export default function App() {
+  return (
+    <div className="App">
+      <MainContainer />
+    </div>
+  );
+}
+
+function MainContainer() {
+  const [itemList, setItemList] = useState(items);
+  const [budget, setBudget] = useState("");
+  const [currentCost, setCurrentCost] = useState(0);
+  function handleAddItem(item) {
+    setItemList((items) => [...items, item]);
+  }
+
+  const totalPrice = itemList.reduce((accumulator, item) => {
+    return accumulator + Number(item.itemPrice);
+  }, 0);
+
+  console.log(totalPrice);
+  return (
+    <div className="main-container">
+      <AddToListForm onAddItem={handleAddItem} />
+      <Totals
+        budget={budget}
+        setBudget={setBudget}
+        totalPrice={totalPrice}
+        currentCost={currentCost}
+      />
+      <Lists
+        itemList={itemList}
+        setItemList={setItemList}
+        currentCost={currentCost}
+        setCurrentCost={setCurrentCost}
+      />
+    </div>
+  );
+}
+
+function AddToListForm({ onAddItem }) {
+  const id = crypto.randomUUID();
+  const [itemName, setItemName] = useState("");
+  const [itemPrice, setItemPrice] = useState("");
+
+  function handleSubmit(e) {
+    if (!itemName || !itemPrice) return;
+    e.preventDefault();
+    const newItem = {
+      id,
+      itemName,
+      itemPrice,
+      purchased: false,
+    };
+    onAddItem(newItem);
+  }
+
+  return (
+    <form className="add-to-list" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Item name..."
+        value={itemName}
+        onChange={(e) => setItemName(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Item price..."
+        value={itemPrice}
+        onChange={(e) => setItemPrice(Number(e.target.value))}
+      />
+
+      <button>Add to list🛒</button>
+    </form>
+  );
+}
+
+function Totals({ budget, setBudget, totalPrice, currentCost }) {
+  function handleBudget(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="totals">
+      <form action="" onSubmit={handleBudget}>
+        <div className="form-group">
+          <label>Budget: </label>
+          <input
+            value={budget}
+            onChange={(e) => setBudget(Number(e.target.value))}
+          />
+        </div>
+        <button>Set budget💵</button>
+      </form>
+      <div className="form-group">
+        <label>My Budget: </label>
+        <input
+          value={typeof budget === "number" ? `R${budget}` : budget}
+          disabled
+        />
+      </div>
+      <div className="form-group">
+        <label>Current Cost: </label>
+        <input value={Math.abs(currentCost).toFixed(2)} />
+      </div>
+      <div className="form-group">
+        <label>Total Price: </label>
+        <input type="text" value={`R${totalPrice}`} disabled />
+      </div>
+    </div>
+  );
+}
+
+function Lists({ itemList, setItemList, currentCost, setCurrentCost }) {
+  return (
+    <div className="list">
+      <ListTable
+        itemList={itemList}
+        setItemList={setItemList}
+        setCurrentCost={setCurrentCost}
+        currentCost={currentCost}
+      />
+    </div>
+  );
+}
+
+function ListTable({ itemList, setItemList, currentCost, setCurrentCost }) {
+  function handlePurchase(item) {
+    const updateItems = itemList.map((i) => {
+      if (i.id === item.id) {
+        setCurrentCost(
+          (currentCost = i.purchased
+            ? currentCost + i.itemPrice
+            : currentCost + -i.itemPrice)
+        );
+        return { ...i, purchased: !i.purchased };
+      }
+      return i;
+    });
+
+    setItemList(updateItems);
+  }
+  return (
+    <table className="">
+      <thead>
+        <tr className="table-head-row">
+          <th>Name</th>
+          <th>Price</th>
+          <th className="bin">Purchased</th>
+        </tr>
+      </thead>
+      <tbody>
+        {itemList.map((item) => (
+          <tr key={item.id}>
+            <td>{item.itemName}</td>
+            <td>R{item.itemPrice}</td>
+            <td onClick={() => handlePurchase(item)} className="item-check">
+              {item.purchased ? "✅" : "❌"}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
